fd0070047d66decd02602651dfed629a39492805
Daphne Nnadi <111007843+Helianthus22@users.noreply.github.com>
2023-09-15 08:44:27 +0100

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,6 +1,3 @@
  BOT_TOKEN = "6696230486:AAHn2IyePbZSG4bPT0xjXlQFeo0oFmSICrw"
 
-CUSTOM_REPLIES = {
-    "hi": "Hello",
-    "how are you?": "I am fine!"
-}
+CUSTOM_REPLIES = {"Hello": "Hi there!", "How are you?": "I'm just a bot, but I'm here to help!", "Goodbye": "Goodbye! If you have more questions, feel free to ask."}
